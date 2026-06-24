dafb336e59448c09831bbaace2035ad99833cdd9
Alexander Velilyaev <alexandervelilyaev@gmail.com>
2026-06-24 19:45:47 +0200

    Created the Hello world network

--- MyNeuralNetworkExperience/MainForm.cs
+++ MyNeuralNetworkExperience/MainForm.cs
@@ -28,5 +28,53 @@ namespace MyNeuralNetworkExperience
         {
             // TODO: use openFileDialog
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            List<Neuron> neuronList = new List<Neuron>();
+            List<Synapse> synapseList = new List<Synapse>();
+
+            Neuron n1 = new Neuron()
+            {
+                Id = 1
+            };
+            neuronList.Add(n1);
+
+            Neuron n2 = new Neuron()
+            {
+                Id = 2
+            };
+            neuronList.Add(n2);
+
+            Neuron n3 = new Neuron()
+            {
+                Id = 3
+            };
+            neuronList.Add(n3);
+
+            Synapse s1 = new Synapse()
+            {
+                Id = 1,
+                SourceId = 1,
+                DestinationId = 3,
+                Weight = new Random().NextDouble()
+            };
+            synapseList.Add(s1);
+
+            Synapse s2 = new Synapse()
+            {
+                Id = 2,
+                SourceId = 2,
+                DestinationId = 3,
+                Weight = new Random().NextDouble()
+            };
+            synapseList.Add(s2);
+
+            NeuralNetwork nn = new NeuralNetwork();
+            nn.Neurons = neuronList;
+            nn.Synapses = synapseList;
+
+            Console.WriteLine("");
+        }
     }
 }
