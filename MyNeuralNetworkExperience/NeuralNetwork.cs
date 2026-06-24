6c02ad7ccfd31fd2b81707316fb648dd1a147f56
Alexander Velilyaev <alexandervelilyaev@gmail.com>
2026-06-24 19:45:22 +0200

    Created the NeuralNetwork type

---
+++ MyNeuralNetworkExperience/NeuralNetwork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNeuralNetworkExperience
+{
+    public class NeuralNetwork
+    {
+        public NeuralNetwork()
+        {
+            Neurons = new List<Neuron>();
+            Synapses = new List<Synapse>();
+        }
+
+        public List<Neuron> Neurons { get; set; }
+        public List<Synapse> Synapses { get; set; }
+    }
+}
