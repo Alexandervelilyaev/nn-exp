af45e8007094a3230d76f277b5adfca0f57bba9f
Alexander Velilyaev <alexandervelilyaev@gmail.com>
2026-06-24 19:07:37 +0200

    Created basic types

---
+++ MyNeuralNetworkExperience/Synapse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNeuralNetworkExperience
+{
+    public class Synapse
+    {
+        public int Id { get; set; }
+
+        public int SourceId { get; set; }
+
+        public int DestinationId { get; set; }
+
+        public double Weight { get; set; }
+    }
+}
