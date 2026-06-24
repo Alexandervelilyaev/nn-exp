d902bc5d1d1cb0da6f880c08219dfe59ff48b228
Alexander Velilyaev <alexandervelilyaev@gmail.com>
2026-06-24 21:19:11 +0200

    Worked on visualization of the neural network

--- MyNeuralNetworkExperience/MainForm.cs
+++ MyNeuralNetworkExperience/MainForm.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Drawing2D;
+
 namespace MyNeuralNetworkExperience
 {
     public partial class MainForm : Form
@@ -29,9 +31,54 @@ namespace MyNeuralNetworkExperience
             // TODO: use openFileDialog
         }
 
+        private void DrawCircle(int x, int y, int radius, Color color, Bitmap image)
+        {
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                using (Pen pen = new Pen(color, 2))
+                {
+                    g.DrawEllipse(pen, x, y, radius, radius);
+                }
+            }
+        }
+
+        private SizeF DrawText(string text, int x, int y, Color color, Bitmap image)
+        {
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                var font = new Font("Tahoma", 8);
+                var size = g.MeasureString(text, font);
+
+                RectangleF rectf = new RectangleF(x, y, size.Width, size.Height);
+
+                rectf.X -= rectf.Width / 2;
+                rectf.Y -= rectf.Height / 2;
+
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                g.DrawString(text, font, Brushes.Black, rectf);
+
+                return size;
+            }
+        }
+
         private void MainForm_Load(object sender, EventArgs e)
         {
             var bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                bitmap.SetPixel(i, 0, Color.Black);
+            }
+
+            for (int i = 0; i < bitmap.Height; i++)
+            {
+                bitmap.SetPixel(0, i, Color.Black);
+            }
 
             //for (int i = 0; i < Math.Min(bitmap.Height, bitmap.Width); i++)
             //{
@@ -81,24 +128,47 @@ namespace MyNeuralNetworkExperience
             nn.Neurons = neuronList;
             nn.Synapses = synapseList;
 
+            List<int> renderedIds = new List<int>();
+
             // TODO: Move this select to the NeuralNetwork class
             List<Neuron> inputNeurons = nn.Neurons
                 .Where(n => !nn.Synapses.Select(s => s.DestinationId).ToList().Contains(n.Id))
                 .ToList();
 
+            int padding = 5;
+            int radius = 20;
+            int x = padding;
+            int y = padding;
 
-
-            using (Graphics g = Graphics.FromImage(bitmap))
+            foreach (Neuron neuron in inputNeurons)
             {
-                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-
-                using (Pen pen = new Pen(Color.Red, 2))
+                if (!renderedIds.Contains(neuron.Id))
                 {
-                    // 4. Draw the circle (X, Y, Width, Height)
-                    // Equal width and height creates a perfect circle
-                    var radius = 300;
-                    g.DrawEllipse(pen, 50, 50, radius, radius);
+                    DrawCircle(x, y, radius, Color.Green, bitmap);
+                    DrawText(neuron.Id.ToString(), x + radius / 2, y + radius / 2, Color.Blue, bitmap);
+                    renderedIds.Add(neuron.Id);
                 }
+
+                // TODO: Move this select to the NeuralNetwork class
+                var synapses = nn.Synapses.Where(s => s.SourceId == neuron.Id).ToList();
+
+                // TODO: Move this select to the NeuralNetwork class
+                List<Neuron> nextLayerNeurons = nn.Neurons
+                    .Where(n => synapses.Select(s => s.DestinationId).ToList().Contains(n.Id))
+                    .ToList();
+
+                foreach (Neuron nextLayerNeuron in nextLayerNeurons)
+                {
+                    if (!renderedIds.Contains(nextLayerNeuron.Id))
+                    {
+                        var xx = x + radius * 2 + padding;
+                        DrawCircle(xx, y, radius, Color.Red, bitmap);
+                        DrawText(nextLayerNeuron.Id.ToString(), xx + radius / 2, y + radius / 2, Color.Blue, bitmap);
+                        renderedIds.Add(nextLayerNeuron.Id);
+                    }
+                }
+
+                y += padding + radius * 2;
             }
 
             pictureBox1.Image = bitmap;
