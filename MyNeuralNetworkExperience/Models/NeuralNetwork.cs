1cc5030225f6dbc3eb24ff58fea47d06892f784a
Alexander Velilyaev <alexandervelilyaev@gmail.com>
2026-07-06 15:11:58 +0200

    Removed unused imports

--- MyNeuralNetworkExperience/Models/NeuralNetwork.cs
+++ MyNeuralNetworkExperience/Models/NeuralNetwork.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyNeuralNetworkExperience.Models
+﻿namespace MyNeuralNetworkExperience.Models
 {
     public class NeuralNetwork
     {
